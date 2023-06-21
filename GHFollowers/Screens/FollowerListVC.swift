5928588c562577a6c05ebdfb7817daac40f1d82e
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2023-06-21 20:08:53 +0530

    Collection view initial setup

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,21 +10,14 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var userName:String!
-
+    var collectionView:UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemBackground
-        
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
-            switch result{
-            case .success(let followers):
-                print(followers)
-            case .failure(let error):
-                self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "ok")
-            }
-        }
+        configureVC()
+        configureCollectionView()
+        getFollowers()
         
     }
     
@@ -33,5 +26,29 @@ class FollowerListVC: UIViewController {
         
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
+    
+    private func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+        
+    }
+    
+    private func configureVC(){
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func getFollowers(){
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+            switch result{
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "ok")
+            }
+        }
+    }
+    
 }
