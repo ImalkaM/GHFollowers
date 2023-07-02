206df6fac71316b8326f5094336ecb39de07d0af
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2023-07-02 20:00:47 +0530

    configure collection view diffable data source, fix iOS 15 related cell issue (not constraining to content view)

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section{
+        case main
+    }
+    
     var userName:String!
     var collectionView:UICollectionView!
+    var dataSource:UICollectionViewDiffableDataSource<Section,Follower>!
+    
+    var followers = [Follower]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,7 @@ class FollowerListVC: UIViewController {
         configureVC()
         configureCollectionView()
         getFollowers()
+        configureDataSource()
         
     }
     
@@ -30,7 +38,7 @@ class FollowerListVC: UIViewController {
     private func configureCollectionView(){
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
     }
@@ -62,11 +70,27 @@ class FollowerListVC: UIViewController {
         NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
             switch result{
             case .success(let followers):
-                print(followers)
+                self.followers = followers
+                self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "ok")
             }
         }
+    }
+    
+    private func configureDataSource(){
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    private func updateData(){
+        var snapshot = NSDiffableDataSourceSnapshot<Section,Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {self.dataSource.apply(snapshot, animatingDifferences: true)}
     }
     
 }
