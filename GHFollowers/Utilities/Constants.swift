20d86142c4de9c8acd5312e1f5a5e12e06363599
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2023-07-30 17:56:49 +0530

    implement user info header section

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Imalka Muthukumara on 2023-07-29.
+//
+
+import Foundation
+
+
+enum SFSymbols{
+    static let location = "mappin.and.ellipse"
+}
