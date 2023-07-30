efe745bd371ab2114e04b405fbbc746c0a96d794
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2023-07-30 18:38:05 +0530

    configure user info bottom items and refactor repetitive ui layout code using a for loops

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -10,21 +10,63 @@ import UIKit
 class UserInfoVC: UIViewController {
     
     let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
     
+    var itemViews:[UIView] = []
     
     var userName:String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
-        
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
-        
-        navigationItem.rightBarButtonItem = doneButton
-        
+        configureVC()
         layoutUI()
+        getUserInfo()
+    
+    }
+    
+    func layoutUI(){
+        itemViews = [headerView,itemViewOne,itemViewTwo]
         
+        let padding:CGFloat = 20
+        let itemHeight:CGFloat = 140
+        
+        
+        //refactor using for loog UI constraints
+        for item in itemViews{
+            view.addSubview(item)
+            item.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                item.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: padding),
+                item.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -padding),
+            ])
+        }
+        
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor = .systemBlue
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
+            
+        ])
+    }
+    
+    private func configureVC(){
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    private func getUserInfo(){
         NetworkManager.shared.getUserInfo(for: userName) { [weak self] result in
             guard let self = self else {return}
             
@@ -39,27 +81,14 @@ class UserInfoVC: UIViewController {
         }
     }
     
-    func layoutUI(){
-        view.addSubview(headerView)
-        
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
-        ])
-    }
-    
-    func add(childVC:UIViewController,to containerView:UIView){
+    private func add(childVC:UIViewController,to containerView:UIView){
         addChild(childVC)
         containerView.addSubview(childVC.view)
         childVC.view.frame = containerView.bounds
         childVC.didMove(toParent: self)
     }
     
-    @objc func dismissVC(){
+    @objc private func dismissVC(){
         dismiss(animated: true)
     }
 }
