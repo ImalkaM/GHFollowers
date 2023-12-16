606117c90aa55cfeb36c73d8987073d927244f1e
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2023-12-16 18:20:47 +0530

    Integrate SwiftUI view into DollowerCell IOS 16 onward

---
+++ GHFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowerView.swift
+//  GHFollowers
+//
+//  Created by Imalka Muthukumara on 2023-12-16.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowerView()
+}
